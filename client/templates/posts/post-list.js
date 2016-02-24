d2fcd34d2743410810c794ad5735b29f0e557588
hassan1709 <hassan.softdeveloper@gmail.com>
2016-02-24 20:45:38 +0800

    Wired collection into 'postsList' template

--- client/templates/posts/post-list.js
+++ client/templates/posts/post-list.js
@@ -1,17 +1,5 @@
-var postsData = [
-	{
-		title: 'Introducing Telescope',
-		url: 'http://sashagreif.com/introducing-telescope/'
-	},
-	{
-		title: 'Meteor',
-		url: 'http://meteor.com'
-	},
-	{
-		title: 'The Meteor Book',
-		url: 'http://themeteorbook.com'
-	}
-];
 Template.postList.helpers({
-	posts: postsData
+	posts: function(){
+		return Posts.find();
+	}
 });
