187e6440ee1e9e4405ea7a6ef3ee355a5d509f93
hassan1709 <hassan.softdeveloper@gmail.com>
2016-03-06 13:42:25 +0800

    Validate post contents when editing

--- client/templates/posts/post-submit.js
+++ client/templates/posts/post-submit.js
@@ -1,3 +1,16 @@
+Template.postSubmit.onCreated(function(){
+	Session.set('postSubmitErrors', {});
+});
+
+Template.postSubmit.helpers({
+	errorMessage: function (field) {
+		return Session.get('postSubmitErrors')[field];
+	},
+	errorClass: function(field){
+		return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
+	}
+});
+
 Template.postSubmit.events({
 	'submit form': function(e){
 		e.preventDefault();
@@ -23,18 +36,5 @@ Template.postSubmit.events({
 
 			Router.go('postPage', {_id: result._id});
 		});			
-	}
-});
-
-Template.postSubmit.onCreated(function(){
-	Session.set('postSubmitErrors', {});
-});
-
-Template.postSubmit.helpers({
-	errorMessage: function (field) {
-		return Session.get('postSubmitErrors')[field];
-	},
-	errorClass: function(field){
-		return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
 	}
 });
