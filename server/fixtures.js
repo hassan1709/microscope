19f0cfa454be98625ae4940041e971bec1f695ef
hassan1709 <hassan.softdeveloper@gmail.com>
2016-03-12 14:31:46 +0800

    Adding basic upvoting algorithm

---
+++ server/fixtures.js
@@ -0,0 +1,13 @@
+// var _posts = Posts.find();
+
+// _posts.forEach(function (post) {
+// 	console.log("postId: "+post._id+" Title: "+post.title+" Author: "+post.author);
+
+// 	var newPost = _.extend(post, {
+// 		upvoters: [],
+// 		votes: 0
+// 	})
+
+
+// 	Posts.update(post._id, {$set: newPost});
+// });
